7a11c807c48b823e0471f07a8d3295358ea7bec2
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2024-02-04 18:43:58 +0900

    update router

--- src/renderer/src/App/App.tsx
+++ src/renderer/src/App/App.tsx
@@ -1,23 +1,19 @@
 import PageRouter from '@renderer/Global/Router/PageRouter';
-import { onMount, type Component } from 'solid-js';
+import { type Component } from 'solid-js';
 
 import "../Global/Styles/Style.scss";
 
-import style from "./App.module.scss";
-import { Router } from '@solidjs/router';
+import { MemoryRouter, Router } from '@solidjs/router';
 import initStorage from '@renderer/Global/Utils/Storage/initStorage';
 
 const App: Component = () => {
-
-    onMount(() => {
-        initStorage();
-    })
+    initStorage();
 
     return (
         <div class="app">
-            <Router>
+            <MemoryRouter>
                 <PageRouter />
-            </Router>
+            </MemoryRouter>
         </div>
     )
 }
